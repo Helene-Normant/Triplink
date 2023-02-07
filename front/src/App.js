a6fa71156dfc1393c72683ec3b411eed72065050
Vikelle <virginie.kellermann@gmail.com>
2023-02-07 16:17:33 +0100

    Add logo

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,10 @@
+import logo from '../src/assets/LOGO_min.png'
 
 function App() {
   return (
     <div className="btn-dark">
          <h3>S'inscrire</h3>
+      <img src={logo} alt={'Logo Triplink'}/>
     </div>
   );
 }
