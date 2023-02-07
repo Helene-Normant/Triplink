570d13adbde84869b046481122918b03a0087fc5
Vikelle <virginie.kellermann@gmail.com>
2023-02-07 17:13:43 +0100

    Creation Header component

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,8 @@
-import logo from '../src/assets/LOGO_min.png'
+import Header from '../src/Components/Header';
 
 function App() {
   return (
-    <div className="btn-dark">
-         <h3>S'inscrire</h3>
-      <img src={logo} alt={'Logo Triplink'}/>
-    </div>
+    <Header />
   );
 }
 
---
+++ front/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/LOGO_min.png';
+
+function Header() {
+  return (
+  <div className="btn-dark">
+  <h3>S'inscrire</h3>
+  <img src={logo} alt={'Logo Triplink'}/>
+  </div>
+  )};
+
+
+export default Header;
