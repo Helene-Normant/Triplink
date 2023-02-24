19fb3f890e2df803b16fe17ac92b1c9ff82996b5
Helene Normant <normanthelene@gmail.com>
2023-02-24 15:06:39 +0100

    Display header

--- front/src/Components/header/Header.jsx
+++ front/src/Components/header/Header.jsx
@@ -3,9 +3,16 @@ import './header.css';
 
 function Header() {
   return (
-  <div className="btn-dark">
-  <h3>S'inscrire</h3>
-  <img src={logo} alt={'Logo Triplink'}/>
+  <div className='container-header'>
+    <div className='logo-header'>
+      <img className='logo-triplink' src={logo} alt={'Logo Triplink'}/></div>
+    <div className='header-title'>
+      <h1 className='main-title'>Triplink</h1>
+      </div>
+    <div className='header-buttons'>
+      <h3 className="btn-light">Se connecter</h3>
+      <hr className="line-buttons"/>
+      <h3 className="btn-dark">S'inscrire</h3></div>
   </div>
   )};
 
