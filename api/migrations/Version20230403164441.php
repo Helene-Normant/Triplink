75f49a7b05ed9ef486f68a865be79b6857bb4590
Marine <marine.lenoir@hotmail.fr>
2023-03-31 16:54:08 +0200

    Feat-init-api-plateform-and-publication-table

--- api/migrations/Version20230403164441.php
+++ api/migrations/Version20230403164441.php
@@ -10,18 +10,17 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230331101447 extends AbstractMigration
+final class Version20230403164441 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Table for posts of users';
+        return '';
     }
 
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE publication (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(500) NOT NULL, country VARCHAR(255) NOT NULL, budget INT DEFAULT NULL, bag_tips VARCHAR(255) DEFAULT NULL, travel_type VARCHAR(255) NOT NULL, picture VARCHAR(800) NOT NULL, travel_partner VARCHAR(255) DEFAULT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', modified_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('DROP TABLE post');
     }
 
     public function down(Schema $schema): void
