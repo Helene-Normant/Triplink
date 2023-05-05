05132fa866284d2d75605e88e334bdc868ad7780
Anne Barois <anne.barois@omnilog.fr>
2023-05-05 09:59:48 +0200

    Add migration description

--- api/migrations/Version20230421095636.php
+++ api/migrations/Version20230421095636.php
@@ -14,7 +14,7 @@ final class Version20230421095636 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Create relation OneToMany between User and Publication';
     }
 
     public function up(Schema $schema): void
