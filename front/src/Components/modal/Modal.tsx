a60bbadd9bf5fd783ce72f46a1b3628689d3f2d6
Virginie Kellermann <virginiek@wecasa.fr>
2023-05-24 16:42:07 +0200

    fix: modal profil

--- front/src/Components/modal/Modal.tsx
+++ front/src/Components/modal/Modal.tsx
@@ -11,35 +11,37 @@ type ModalProps = {
 };
 
 const Modal = ({ open, onClose }: ModalProps) => {
+  const handleClick = () => { }
+
   if (!open) return null;
   return (
     <div className="overlay">
-    <div className="modal-container">
-      <div onClick={onClose} className="close-modal">
-        <p className="close-btn">
-          {" "}
-          <IoCloseCircleOutline />{" "}
-        </p>
+      <div className="modal-container">
+        <div onClick={onClose} className="close-modal">
+          <p className="close-btn">
+            {" "}
+            <IoCloseCircleOutline />{" "}
+          </p>
+        </div>
+        <div className="modal-form">
+          <h2>Bienvenue sur Triplink</h2>
+          <h3 className="subtitle-modal">J'ai un compte</h3>
+          <form className="form1">
+            <div className='login'>
+              <Input className='input input--large' type="text" placeholder="Adresse email" size="large" />
+            </div>
+            <div className='login'>
+              <Input className='input input--large' type="text" placeholder="Mot de passe" size="large" />
+            </div>
+          </form>
+          <h3 className="lien-modal1">mot de passe oublié</h3>
+        </div>
+        <div className="modal-btn">
+          <Button onClick={handleClick} className="light" children="Se connecter" />
+          <h3 className="lien-modal2"> Pas encore de compte ? </h3>
+          <Link to='/inscription'><h3 className="lien-modal3">Créer un compte en cliquant ici </h3></Link>
+        </div>
       </div>
-      <div className="modal-form">
-        <h2>Bienvenue sur Triplink</h2>
-        <h3 className="subtitle-modal">J'ai un compte</h3>
-        <form className="form1">
-          <div className='login'>
-            <Input className='input input--large' type="text" placeholder="Adresse email" size="large"/>
-          </div>
-          <div className='login'>
-            <Input className='input input--large' type="text" placeholder="Mot de passe" size="large"/>
-          </div>
-        </form>   
-        <h3 className="lien-modal1">mot de passe oublié</h3>
-      </div>
-      <div className="modal-btn">
-      <Button className="light" children="Se connecter" />
-        <h3 className="lien-modal2"> Pas encore de compte ? </h3>
-      <Link to='/inscription'><h3 className="lien-modal3">Créer un compte en cliquant ici </h3></Link>
-      </div>
-    </div>
     </div>
   );
 };
