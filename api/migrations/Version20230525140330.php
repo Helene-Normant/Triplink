4590f9de9fccd350ea4ec431e5d22675a1dcd5c0
Anne Barois <anne.barois@omnilog.fr>
2023-05-25 16:54:50 +0200

    Feat-Create-table-country

---
+++ api/migrations/Version20230525140330.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230525140330 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create country table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, code INT NOT NULL, alpha2 VARCHAR(10) NOT NULL, alpha3 VARCHAR(10) NOT NULL, name_fr VARCHAR(255) NOT NULL, name_en VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE country');
+    }
+}
---
+++ api/src/Entity/Country.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use App\Repository\CountryRepository;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CountryRepository::class)]
+#[ApiResource (
+    operations: [
+        new Get(
+            normalizationContext: [
+                'groups' => ['country:read'],
+            ],
+        ),
+        new GetCollection(),
+    ],
+    normalizationContext: [
+        'groups' => ['country:read'],
+    ]
+    )]
+
+class Country
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['country:read'])]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $code = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $alpha2 = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $alpha3 = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['country:read'])]
+    private ?string $nameFr = null;
+
+    #[ORM\Column(length: 255)]
+    #[Groups(['country:read'])]
+    private ?string $nameEn = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function setCode(int $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getAlpha2(): ?string
+    {
+        return $this->alpha2;
+    }
+
+    public function setAlpha2(string $alpha2): self
+    {
+        $this->alpha2 = $alpha2;
+
+        return $this;
+    }
+
+    public function getAlpha3(): ?string
+    {
+        return $this->alpha3;
+    }
+
+    public function setAlpha3(string $alpha3): self
+    {
+        $this->alpha3 = $alpha3;
+
+        return $this;
+    }
+
+    public function getNameFr(): ?string
+    {
+        return $this->nameFr;
+    }
+
+    public function setNameFr(string $nameFr): self
+    {
+        $this->nameFr = $nameFr;
+
+        return $this;
+    }
+
+    public function getNameEn(): ?string
+    {
+        return $this->nameEn;
+    }
+
+    public function setNameEn(string $nameEn): self
+    {
+        $this->nameEn = $nameEn;
+
+        return $this;
+    }
+}
