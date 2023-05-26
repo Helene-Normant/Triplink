c9325d52877a8937180d8d631f4e854a31f1b4b1
Anne Barois <anne.barois@omnilog.fr>
2023-05-26 14:55:35 +0200

    Add migration to import country datas in DB

---
+++ api/migrations/Version20230526121228.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use Doctrine\DBAL\Connection;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230526121228 extends AbstractMigration
+{
+
+    protected $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function getDescription(): string
+    {
+        return 'Add data to Table country';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $sqlFilePath = __DIR__.'/../src/Data/country_202305261138.sql';
+        $sql = file_get_contents($sqlFilePath);
+    
+        // Execute the SQL query
+        $this->connection->executeQuery($sql);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // Truncate (empty) the table instead of dropping it     
+        $this->connection->executeQuery('TRUNCATE TABLE country');
+    }
+}
