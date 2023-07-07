42038315476c3535637b8e6524d353184bc7a241
Virginie <98782429+Vikelle@users.noreply.github.com>
2023-07-07 14:33:05 +0200

    feat: fix error toast signup (#52)

--- front/src/Pages/inscription/Inscription.tsx
+++ front/src/Pages/inscription/Inscription.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../../Components/input/Input";
 import "./inscription.css";
@@ -9,9 +9,9 @@ import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
 
-const initialUser= {userName:"", userSurname:"", userPseudo:"", userBirth:"", email:"", password:"", userConfirm:""};
+const initialUser = { userName: "", userSurname: "", userPseudo: "", userBirth: "", email: "", password: "", userConfirm: "" };
 const Inscription = () => {
-const [user, setUser] = useState(initialUser);
+  const [user, setUser] = useState(initialUser);
 
   const navigate = useNavigate();
   const handleClick = () => {
@@ -26,36 +26,55 @@ const [user, setUser] = useState(initialUser);
     }));
   };
 
-
-  const signUp = async (event : any) => {
+  const signUp = async (event: any) => {
     event.preventDefault();
+
+    if (user.password !== user.userConfirm) {
+      toast.error("Oups, les mots de passe ne sont pas identiques", {
+        hideProgressBar: true,
+        position: toast.POSITION.BOTTOM_LEFT,
+        icon: "😓",
+      });
+    };
+
     try {
-      if(user.userName && user.userSurname && user.userPseudo && user.userBirth && user.email && user.password) {
-        if(user.password === user.userConfirm) {
-        const registerData = await apiService.User.post({firstName:user.userName, lastName: user.userSurname, username:user.userPseudo, birthday:user.userBirth, email:user.email, plainPassword:user.password});
+      if (user.userName && user.userSurname && user.userPseudo && user.userBirth && user.email && user.password) {
+        if (user.password === user.userConfirm) {
+          const registerData = await apiService.User.post({
+            firstName: user.userName,
+            lastName: user.userSurname,
+            username: user.userPseudo,
+            birthday: user.userBirth,
+            email: user.email,
+            plainPassword: user.password
+          });
 
-        toast.success("Bienvenu.e sur Triplink !", {
-          hideProgressBar: true,
-          position: toast.POSITION.BOTTOM_RIGHT,
-          icon: "🏝️",
-
-           });
-
-      if (registerData)
-      {
-        setUser(initialUser);
-        navigate('/')
+          if (registerData.ok === false) {
+            console.log('ERROR', Error);
+            toast.error("Oups une erreur s'est produite", {
+              hideProgressBar: true,
+              position: toast.POSITION.BOTTOM_LEFT,
+              icon: "😥",
+            });
+          } else {
+            toast.success("Bienvenue sur Triplink !", {
+              hideProgressBar: true,
+              position: toast.POSITION.BOTTOM_RIGHT,
+              icon: "🏝️",
+            });
+            setUser(initialUser);
+            navigate('/');
+          }
+        }
       }
-      }
-    
-    }} catch(error: any ) {
-     toast.error("Oups une erreur s'est produite", {
-      hideProgressBar: true,
-      position: toast.POSITION.BOTTOM_LEFT
-       });
-      }
-  }
-
+    } catch (Error) {
+      console.log('ERROR', Error);
+      toast.error("Oups une erreur s'est produite", {
+        hideProgressBar: true,
+        position: toast.POSITION.BOTTOM_LEFT
+      });
+    }
+  };
 
   return (
     <section className='container-inscription' data-testid="inscription-page">
@@ -63,36 +82,36 @@ const [user, setUser] = useState(initialUser);
         <Link to='/'><h1 className="main-title-inscription">Triplink</h1></Link>
         <p className='welcome'>Bienvenue !</p>
         <p className='join-us'>Rejoins la communauté des voyageurs</p>
-        <form className="form" onSubmit={signUp }>
+        <form className="form" onSubmit={signUp}>
           <div className='first-line'>
             <Input onChange={handleUserChange} value={user.userName} name="userName" className="input input--small" type="text" placeholder="Prénom" size="small" required />
             <Input onChange={handleUserChange} value={user.userSurname} name="userSurname" className="input input--small" type="text" placeholder="Nom" size="small" required />
           </div>
           <div className='second-line'>
-            <Input  onChange={handleUserChange} value={user.userPseudo} name="userPseudo" className="input input--small" type="text" placeholder="Pseudo" size="small" required />
-            <Input  onChange={handleUserChange} value={user.userBirth} name="userBirth" className="input input--small" type="date" placeholder="26/12/2022" size="small" />
+            <Input onChange={handleUserChange} value={user.userPseudo} name="userPseudo" className="input input--small" type="text" placeholder="Pseudo" size="small" required />
+            <Input onChange={handleUserChange} value={user.userBirth} name="userBirth" className="input input--small" type="date" placeholder="26/12/2022" size="small" />
           </div>
           <div>
-            <Input  onChange={handleUserChange} value={user.email} name="email" className="input input--large" type="text" placeholder="Adresse email" size="large" required />
+            <Input onChange={handleUserChange} value={user.email} name="email" className="input input--large" type="text" placeholder="Adresse email" size="large" required />
           </div>
           <div>
-            <Input  onChange={handleUserChange} value={user.password} name="password" className="input input--large" type="password" placeholder="Mot de passe" size="large" required/>
+            <Input onChange={handleUserChange} value={user.password} name="password" className="input input--large" type="password" placeholder="Mot de passe" size="large" required />
           </div>
           <div>
-            <Input  onChange={handleUserChange} value={user.userConfirm} name="userConfirm" className="input input--large" type="password" placeholder="Confirmation de mot de passe" size="large" required />
+            <Input onChange={handleUserChange} value={user.userConfirm} name="userConfirm" className="input input--large" type="password" placeholder="Confirmation de mot de passe" size="large" required />
           </div>
           <div className='conditions-container'>
-          <input type="checkbox" />
-          <h3 className="conditions">J’ai lu et j’accepte les conditions générales d’utilisation</h3>
-        </div>
-        <div className='container-bottom'>
-        <div className="buttons-inscription">
-          <Button onClick={handleClick} className="light" children="Annuler" role="annule" type="button" />
-          <hr className="line-buttons" />
-          <Button  className="dark" children="S'inscrire" role="inscrit" type="submit" />
-        </div>
-        <Link to='/'><img className="logo-triplink-inscription" src={logo} alt={"Logo Triplink"} /></Link>
-      </div>
+            <input type="checkbox" />
+            <h3 className="conditions">J’ai lu et j’accepte les conditions générales d’utilisation</h3>
+          </div>
+          <div className='container-bottom'>
+            <div className="buttons-inscription">
+              <Button onClick={handleClick} className="light" children="Annuler" role="annule" type="button" />
+              <hr className="line-buttons" />
+              <Button className="dark" children="S'inscrire" role="inscrit" type="submit" />
+            </div>
+            <Link to='/'><img className="logo-triplink-inscription" src={logo} alt={"Logo Triplink"} /></Link>
+          </div>
         </form>
       </div>
     </section>
