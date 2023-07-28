c1748162304e680e755034d47d429d35ee7bb1e2
Helene-N <98808947+Helene-Normant@users.noreply.github.com>
2023-07-28 10:31:20 +0200

    Create loading state component (#60)

---
+++ front/src/Components/loading/Loading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './loading.css';
+
+const Loading = () => {
+    return (
+        <div className="loading-container">
+        <span className="loading-2">
+          <span></span>
+        </span>
+      </div>
+    )
+  }
+  
+  export default Loading;
