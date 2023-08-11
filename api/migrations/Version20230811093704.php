db42b6908cde68514e689a77b951d4b936edbba5
Marine <marine.lenoir@hotmail.fr>
2023-08-11 11:48:25 +0200

    Feature-creation-TravelStep-Table

---
+++ api/migrations/Version20230811093704.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230811093704 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Creation of ManyToOne relation between TravelStep and Publication tables';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE travel_step ADD publication_related_id INT NOT NULL');
+        $this->addSql('ALTER TABLE travel_step ADD CONSTRAINT FK_88E11D912F1585E0 FOREIGN KEY (publication_related_id) REFERENCES publication (id)');
+        $this->addSql('CREATE INDEX IDX_88E11D912F1585E0 ON travel_step (publication_related_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE travel_step DROP FOREIGN KEY FK_88E11D912F1585E0');
+        $this->addSql('DROP INDEX IDX_88E11D912F1585E0 ON travel_step');
+        $this->addSql('ALTER TABLE travel_step DROP publication_related_id');
+    }
+}
