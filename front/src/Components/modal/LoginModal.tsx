342b5bf876b930e4d7410e8b63460815f1be5e67
Anne Barois <anne.barois@omnilog.fr>
2023-09-22 17:46:23 +0200

    FEATURE-Setup useContext with user login

--- front/src/Components/modal/LoginModal.tsx
+++ front/src/Components/modal/LoginModal.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import "./login-modal.css";
+import React, { useState, useContext } from "react";
+import { UserContext } from "../../context/userContext";
+import "./modal.css";
 import { Link } from "react-router-dom";
 import { IoCloseCircleOutline } from "react-icons/io5";
 import Input from "../input/Input";
@@ -15,16 +16,18 @@ type ModalProps = {
 
 const initialUser = { password: "", email: "" };
 
-const LoginModal = ({ isOpen, onClose }: ModalProps) => {
-  const handleClick = () => {
-  }
+const Modal = ({ open, onClose }: ModalProps) => {
+  const [userLogin, setUserLogin] = useState(initialUser);
+  const userContext = useContext(UserContext);
 
-  const [user, setUser] = useState(initialUser);
-  // const [username, setUsername] = useState('');
+  if (!userContext) {
+    return null;
+  }
+  const { saveUser } = userContext;
 
   const handleChange = ({ target }: any) => {
     const { name, value } = target
-    setUser((currentUser) => ({
+    setUserLogin((currentUser) => ({
       ...currentUser,
       [name]: value,
     }));
@@ -33,27 +36,29 @@ const LoginModal = ({ isOpen, onClose }: ModalProps) => {
   const handleLogin = async (event: any) => {
     event.preventDefault();
     try {
-      if (user.email && user.password) {
-        const loginData = await apiService.Login.post({ "email": user.email, "password": user.password });
-        if (loginData.hasOwnProperty("token") && loginData.token) {
+      if(userLogin.email && userLogin.password) {
+        const loginData = await apiService.Login.post({"email": userLogin.email, "password": userLogin.password }); 
+        if (loginData.hasOwnProperty("token") && loginData.token) { 
           localStorage.setItem("apiToken", loginData.token);
         }
         if (loginData.hasOwnProperty("userID") && loginData.userID) {
           localStorage.setItem("userID", loginData.userID);
         }
         const name = await apiService.User.get(loginData.userID);
-        if (name.username) {
-          onClose()
-          toast.success("Prêt.e pour le voyage " + name.username + "?", {
-            hideProgressBar: true,
-            position: toast.POSITION.BOTTOM_RIGHT,
-            icon: "🏝️",
-          });
-          setTimeout(() => {
-            window.location.reload();
-          }, 1000);
+        // console.log(name);
+          if (name?.username) { 
+            saveUser({
+              id: loginData.userID, // Assurez-vous de récupérer l'ID de l'utilisateur correctement
+              username: name.username,
+              token: loginData.token, // Assurez-vous de récupérer le jeton correctement
+            });
+            onClose()
+            toast.success("Prêt.e pour le voyage "  + name.username + "?", {
+              hideProgressBar: true,
+              position: toast.POSITION.BOTTOM_RIGHT,
+              icon: "🏝️",
+            });
         }
-        console.log(loginData.userID)
       }
     } catch (error: any) {
       toast.error("Oups il y a une erreur", {
@@ -64,7 +69,7 @@ const LoginModal = ({ isOpen, onClose }: ModalProps) => {
     }
   };
 
-  if (!isOpen) return null;
+  if (!open) return null;
   return (
     <div className="overlay">
       <div className="modal-container">
@@ -79,18 +84,18 @@ const LoginModal = ({ isOpen, onClose }: ModalProps) => {
             <h2>Bienvenue sur Triplink</h2>
             <h3 className="subtitle-modal">J'ai un compte</h3>
             <div className='login'>
-              <Input className='input input--large' onChange={handleChange} type="text" value={user.email} name="email" placeholder="Adresse email" size="large" required />
+              <Input className='input input--large' onChange={handleChange} type="text" value={userLogin.email} name="email" placeholder="Adresse email" size="large" required />
             </div>
             <div className='login'>
-              <Input className='input input--large' onChange={handleChange} type="password" value={user.password} name="password" placeholder="Mot de passe" size="large" required />
+              <Input className='input input--large' onChange={handleChange} type="password" value={userLogin.password} name="password" placeholder="Mot de passe" size="large" required/>
             </div>
-            <h3 className="lien-modal1">mot de passe oublié</h3>
-          </div>
-          <div className="modal-btn">
-            <Button onClick={handleClick} className="light" children="Se connecter" type='submit' role="login" />
-            <h3 className="lien-modal2"> Pas encore de compte ? </h3>
-            <Link to='/inscription'><h3 className="lien-modal3">Créer un compte en cliquant ici </h3></Link>
-          </div>
+          <h3 className="lien-modal1">mot de passe oublié</h3>
+        </div>
+        <div className="modal-btn">
+          <Button className="light" children="Se connecter" type='submit' role="login" />
+          <h3 className="lien-modal2"> Pas encore de compte ? </h3>
+          <Link to='/inscription'><h3 className="lien-modal3">Créer un compte en cliquant ici </h3></Link>
+        </div>
         </form>
       </div>
     </div>
