c0dacdd1548329ed67109cd5f389ae1f6bc68233
Helene Normant <normanthelene@gmail.com>
2023-10-06 15:10:00 +0200

    Add return link home page

--- front/src/Pages/Details.tsx
+++ front/src/Pages/Details.tsx
@@ -3,17 +3,29 @@ import Header from '../Components/header/Header';
 import TravelDetail from "../Components/details/TravelDetail";
 import PhotoDetail from "../Components/details/PhotoDetail";
 import './details.css'
+import { BsArrowReturnLeft } from "react-icons/bs";
+import { Link} from "react-router-dom";
 
 const Details = () => {
   return (
     <>
       <Header/>
+      <div className="card-details-return"> 
+        <Link className="link-return" to="/">
+         <div className="icon-return">
+          <h1><BsArrowReturnLeft/></h1>
+        </div>
+        <div className="return-details">
+          <h2>Retour</h2>
+        </div>
+       </Link>
+      </div>
       <div className="card-details-container">
        <PhotoDetail/>
        <hr className="line-details"></hr>
        <TravelDetail/>
       </div>
-      </>
+   </>
   )
 }
 
