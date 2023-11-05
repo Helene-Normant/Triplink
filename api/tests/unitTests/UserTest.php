be7fde02c0f9a61bece46c7b31cfb2bc6307845d
Vikelle <virginie.kellermann@gmail.com>
2023-11-05 19:20:14 +0100

    WIP

--- api/tests/unitTests/UserTest.php
+++ api/tests/unitTests/UserTest.php
@@ -6,73 +6,72 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    public function testCreateUser()
-    {
-        $user = new User();
-        $user->setUsername('john_doe');
-        $user->setEmail('john@example.com');
-        $user->setPassword('password123');
+  public function testCreateUser()
+  {
+    $user = new User();
+    $user->setUsername('john_doe');
+    $user->setEmail('john@example.com');
+    $user->setPassword('password123');
 
-        $this->assertInstanceOf(User::class, $user);
-        $this->assertEquals('john_doe', $user->getUsername());
-        $this->assertEquals('john@example.com', $user->getEmail());
-        $this->assertEquals(['ROLE_USER'], $user->getRoles()); // Assuming default role is 'ROLE_USER'
-    }
+    $this->assertInstanceOf(User::class, $user);
+    $this->assertEquals('john_doe', $user->getUsername());
+    $this->assertEquals('john@example.com', $user->getEmail());
+    $this->assertEquals(['ROLE_USER'], $user->getRoles()); // Assuming default role is 'ROLE_USER'
+  }
 
 
 
-    public function testAddAndRemovePublication()
-    {
-        $user = new User();
-        $user->setUsername('jane_doe');
-        $user->setEmail('jane@example.com');
-        $user->setPassword('password456');
+  public function testAddAndRemovePublication()
+  {
+    $user = new User();
+    $user->setUsername('jane_doe');
+    $user->setEmail('jane@example.com');
+    $user->setPassword('password456');
 
-        $publication = new Publication();
-        $publication->setTitle('Sample Publication');
-        $publication->setDescription('This is a test publication.');
+    $publication = new Publication();
+    $publication->setTitle('Sample Publication');
+    $publication->setDescription('This is a test publication.');
 
-        $user->addPublication($publication);
+    $user->addPublication($publication);
 
-        $this->assertTrue($user->getPublications()->contains($publication));
+    $this->assertTrue($user->getPublications()->contains($publication));
 
-        $user->removePublication($publication);
+    $user->removePublication($publication);
 
-        $this->assertFalse($user->getPublications()->contains($publication));
-    }
+    $this->assertFalse($user->getPublications()->contains($publication));
+  }
 
-    public function testSetAndGetRoles()
-    {
-        $user = new User();
-        $roles = ['ROLE_ADMIN', 'ROLE_USER'];
+  public function testSetAndGetRoles()
+  {
+    $user = new User();
+    $roles = ['ROLE_ADMIN', 'ROLE_USER'];
 
-        $user->setRoles($roles);
+    $user->setRoles($roles);
 
-        $this->assertEquals($roles, $user->getRoles());
-    }
+    $this->assertEquals($roles, $user->getRoles());
+  }
 
-    public function testAddAndRemoveLikedPublication()
-    {
-        $user = new User();
-        $publication = new Publication();
+  public function testAddAndRemoveLikedPublication()
+  {
+    $user = new User();
+    $publication = new Publication();
 
-        $user->addLikedPublication($publication);
+    $user->addLikedPublication($publication);
 
-        $this->assertTrue($user->getLikedPublications()->contains($publication));
+    $this->assertTrue($user->getLikedPublications()->contains($publication));
 
-        $user->removeLikedPublication($publication);
+    $user->removeLikedPublication($publication);
 
-        $this->assertFalse($user->getLikedPublications()->contains($publication));
-    }
+    $this->assertFalse($user->getLikedPublications()->contains($publication));
+  }
 
-    public function testSetAndGetPassword()
-    {
-        $user = new User();
-        $password = 'new_password';
+  public function testSetAndGetPassword()
+  {
+    $user = new User();
+    $password = 'new_password';
 
-        $user->setPassword($password);
+    $user->setPassword($password);
 
-        $this->assertEquals($password, $user->getPassword());
-    }
-
+    $this->assertEquals($password, $user->getPassword());
+  }
 }
