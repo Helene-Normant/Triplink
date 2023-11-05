2c23479d430e1982dc67f92678f4638a69d0ae3c
Vikelle <virginie.kellermann@gmail.com>
2023-11-05 12:37:10 +0100

    WIP

--- api/migrations/Version20230525140330.php
+++ api/migrations/Version20230525140330.php
@@ -7,25 +7,27 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230525140330 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return 'Create country table';
-    }
+  public function getDescription(): string
+  {
+    return 'Create country table';
+  }
 
-    public function up(Schema $schema): void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, code INT NOT NULL, alpha2 VARCHAR(10) NOT NULL, alpha3 VARCHAR(10) NOT NULL, name_fr VARCHAR(255) NOT NULL, name_en VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-    }
+  public function up(Schema $schema): void
+  {
+    $this->addSql('CREATE TABLE country (
+          id INT AUTO_INCREMENT NOT NULL,
+          code INT NOT NULL,
+          alpha2 VARCHAR(10) NOT NULL,
+          alpha3 VARCHAR(10) NOT NULL,
+          name_fr VARCHAR(255) NOT NULL,
+          name_en VARCHAR(255) NOT NULL,
+          PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+  }
 
-    public function down(Schema $schema): void
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE country');
-    }
+  public function down(Schema $schema): void
+  {
+    $this->addSql('DROP TABLE country');
+  }
 }
